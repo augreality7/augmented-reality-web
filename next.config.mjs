18e2f69fe37313a8c1bced0c15cfc7aaa0e90c5d
unknown <tabajondabryan7@gmail.com>
2024-07-21 18:27:50 +0800

    fix the fs and encoding errong

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
     images: {
         domains: ['firebasestorage.googleapis.com'],
     },
+    webpack(config, { isServer }) {
+        // Add fallback for the 'fs' module
+        if (!isServer) {
+          config.resolve.fallback = {
+            fs: false,
+          };
+        }
+        return config;
+      },
 };
 
 export default nextConfig;
